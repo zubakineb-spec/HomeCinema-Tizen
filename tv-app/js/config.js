f031a3bafac6e6fd15ba2d0920f416608875f7c7
zubakineb-spec <zubakineb@inbox.ru>
2026-08-08 20:17:55 +0500

    Add Samsung Tizen TV client for v0.1.0

---
+++ tv-app/js/config.js
@@ -0,0 +1,3 @@
+// When the UI is served by the backend in a browser, leave empty.
+// For an installed Tizen package, set the LAN address of the Home Cinema backend.
+window.HOME_CINEMA_API = window.HOME_CINEMA_API || 'http://192.168.0.101:8096';
